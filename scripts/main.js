28532418945fe3b763a2d1b5bd5d8223c531f7da
BrendonPierson <brendonpierson@gmail.com>
2015-07-21 11:48:36 -0500

    solved

--- scripts/main.js
+++ scripts/main.js
@@ -3,11 +3,19 @@
   passed in as argument. Return the product.
  */
 
+function multiply(x, y) {
+ 	return (x * y);
+ }
+
 
 /*
   Create a function that divides two numbers
   passed in as argument. Return the quotient.
  */
+
+function divide(x, y) {
+ 	return (x / y);
+ }
 
 
 /*
@@ -17,7 +25,13 @@
   e.g. var result = myFunction(0, 0, func)
  */
 
+function result(x, y, func){
+ 	return func(x, y);
+ }
+
 
 /*
   Log the result
  */
+
+ console.log(result(10, 3, multiply));
